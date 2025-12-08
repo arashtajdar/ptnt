cc2d18d1043ac189bdc05374308935f989960061
Arash Tajdar <atajdar@theearplatform.com>
2025-12-08 20:49:42 +0100

    Update

--- database/migrations/2025_11_04_120924_create_questions_table.php
+++ database/migrations/2025_11_04_120924_create_questions_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('questions', function (Blueprint $table) {
             $table->id();
             $table->string('text');
+            $table->string('text_fa');
             $table->string('image')->nullable();
             $table->string('answer', 1); // V or F
             $table->integer('parent_number')->nullable();
